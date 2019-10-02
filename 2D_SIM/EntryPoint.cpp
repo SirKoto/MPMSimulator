cf171edddce0ba72bdf483aa7e7886c3ec3bc614
tonisusin <toni.susin@upc.edu>
2019-10-02 13:03:31 +0200

    Versio OpenGL3.3 per Toni

--- 2D_SIM/EntryPoint.cpp
+++ 2D_SIM/EntryPoint.cpp
@@ -38,8 +38,8 @@ Shader shader;
 bool initGLFW(GLFWwindow *&window)
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(utils::SCR_WIDTH, utils::SCR_HEIGHT, "2D_SIM", NULL, NULL);
