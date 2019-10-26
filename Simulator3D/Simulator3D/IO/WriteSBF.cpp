0bec72e0522cdfb2ddcf336dd3aecae585a2e54a
Pol Martín <pol147258369@gmail.com>
2019-10-26 20:10:15 +0200

    Added writter to SBF files

---
+++ Simulator3D/Simulator3D/IO/WriteSBF.cpp
@@ -0,0 +1,53 @@
+#include "WriteSBF.h"
+
+WriteSBF::WriteSBF(const std::string& path, const unsigned long n_particles) :
+	stream(path.c_str(), std::ios::out | std::ios::trunc),
+	n(n_particles), rest(n_particles % size_bulk), it(n_particles / size_bulk)
+{
+	// write number of particles
+	{
+		char data[sizeof(n_particles)];
+		std::memcpy(data, &n_particles, sizeof(n_particles));
+		stream.write(data, sizeof(n_particles));
+	}
+	stream.flush();
+
+
+}
+
+WriteSBF::~WriteSBF()
+{
+	// write EOF 
+	{
+		char flag = 0x00;
+		stream.write(&flag, 1);
+	}
+	stream.flush();
+	stream.close();
+}
+
+void WriteSBF::writeData3f(const float* data)
+{
+	char bloat[sizeof(float) * 3 * size_bulk];
+
+	// write some flag at the start
+	{
+		char flag = 0xFF;
+		stream.write(&flag, 1);
+	}
+
+	// copy data
+	unsigned long i;
+	for (i = 0; i < it; ++i)
+	{
+		// use pointer arithmetic to add to the float array
+		std::memcpy(bloat, data + i, sizeof(float) * 3 * size_bulk);
+		stream.write(bloat, sizeof(float) * 3 * size_bulk);
+	}
+
+	// copy the rest
+	std::memcpy(bloat, data + i, sizeof(float) * 3 * rest);
+	stream.write(bloat, sizeof(float) * 3 * rest);
+
+	stream.flush();
+}
---
+++ Simulator3D/Simulator3D/IO/WriteSBF.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef _WRITESBF_
+#define _WRITESBF_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class WriteSBF
+{
+public:
+	WriteSBF(const std::string& path, const unsigned long n_particles);
+	
+	~WriteSBF();
+
+	void writeData3f(const float* data);
+
+private:
+
+	static constexpr unsigned int size_bulk = 100;
+
+	const unsigned long n;
+	const unsigned long rest;
+	const unsigned long it;
+
+
+	std::ofstream stream;
+
+};
+
+#endif // !_WRITESBF_
